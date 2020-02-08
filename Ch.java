6e9308f6319689b64d7ceba379af0e79fa1fa95d
dedeepyasadineni <60135031+dedeepyasadineni@users.noreply.github.com>
2020-02-08 22:32:39 +0530

    Add files via upload

---
+++ Ch.java
@@ -0,0 +1,16 @@
+package newyeargift;
+
+public class Ch {
+   int weight,price;
+   Ch(int weight,int price)
+   {
+	   this.weight=weight;
+	   this.price=price;
+   }
+   public int getPrice() {
+		return price;
+	}
+	public int getWeight() {
+		return weight;
+	}
+}
---
+++ Sw.java
@@ -0,0 +1,16 @@
+package newyeargift;
+
+public class Sw{
+	int weight ,price;
+	Sw(int weight, int price)
+	{
+		this.weight=weight;
+		this.price=price;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getWeight() {
+		return weight;
+	}
+}
